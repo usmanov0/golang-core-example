e6d21510cb7159107ac36b29b82de44109fce719
usmanov <azikusmanov882@gmail.com>
2024-03-13 16:23:56 +0500

    Functions

---
+++ 7-lesson/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a int = 10
+	var b int = 20
+	fmt.Println(a, b)
+	//swap(a, b)
+	swapByref(&a, &b)
+	fmt.Println(a, b)
+
+	// result := sum(1, 2)
+	// fmt.Println(result)
+	/* result, err := sqrt(-5)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	} */
+}
+
+func swap(x, y int) int {
+	var o int
+	o = x
+	x = y
+	y = o
+	return o
+}
+
+func swapByref(x, y *int) int {
+	var o int
+	o = *x
+	*x = *y
+	*y = o
+	return o
+}
+
+func sum(x int, y int) int {
+	return x + y
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Not clear for a negative number")
+	}
+	return math.Sqrt(x), nil
+}
